aade4175edc26e95649c52ce1c73d9bad1ac851a
valjamettduoc <val.jamett@duocuc.cl>
2024-09-10 19:24:25 -0300

    Insertando base de App Alumnos

---
+++ AppAlumnos/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppAlumnos',
+  webDir: 'www'
+};
+
+export default config;
