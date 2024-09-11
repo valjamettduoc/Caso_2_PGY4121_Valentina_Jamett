5cf1bb731a41a5407a64a8b4085e6f210face764
valjamettduoc <val.jamett@duocuc.cl>
2024-09-11 00:53:24 -0300

    commit para registro
    Se me olvidó hacer el commit de a poco.

---
+++ AppAlumnos/src/app/inicio/inicio.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  constructor( private router: Router) { }
+
+  redireccionPagina() {
+    this.router.navigate(['/']); // Cambia '/other-page' al path de la página destino
+  }
+
+  ngOnInit() {
+  }
+
+}
